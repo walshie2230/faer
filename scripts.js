a98db21a10df882a75a9ec2f499b8830428815f2
walshie2230 <68523282+walshie2230@users.noreply.github.com>
2024-07-24 09:33:31 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const days = document.querySelectorAll('.day');
-
     days.forEach(day => {
         day.addEventListener('click', function() {
             day.classList.toggle('expanded');
         });
-
         const closeButton = day.querySelector('.close-button');
         closeButton.addEventListener('click', function(event) {
             event.stopPropagation();
